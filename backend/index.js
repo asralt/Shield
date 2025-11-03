da60dc4d6d8be6e5bdb67c50f21540da484037a2
aska <enkhbadralasral00@gmail.com>
2025-11-03 00:50:19 +0000

    ADDED db ADDED mongodb dependency to backend package.json

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { connect } from 'mongoose';
+import connectDB from './config/db';
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Shield Backend is running');
