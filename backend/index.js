947aef7a9684b72fa40e9e9151535c8b4394099b
aska <enkhbadralasral00@gmail.com>
2025-11-11 19:22:21 +0000

    `Added MONGO_URI to .env file and updated index.js to import express and connect to MongoDB`

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connect } from 'mongoose';
-import connectDB from './config/db';
+import connectDB from './config/db.js';
+import express from 'express';
 
 dotenv.config();
 const app = express();
@@ -10,13 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-connectDB();
 
 app.get('/', (req, res) => {
     res.send('Shield Backend is running');
 });
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
